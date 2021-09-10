ac68a241efbb2624ddbd864e62a99c1ea9e6a033
gljivan <duspic77@gmail.com>
2021-09-10 14:50:37 +0200

    Replaced the conv_block  function with a class ConvBlock This takes care of the former problem of instantiating a new nn.Conv2d layer each time InceptionNet.foward() is called
    Now, the Conv2d layer an attribute of Conv2d ojbects (*instance attribute*) and is instantiated only once per object.
    Fowarding data trough layers is done by ConvBlock.fwd(), which is separated from the initialization.

--- InceptionNet_v4.py
+++ InceptionNet_v4.py
@@ -1,22 +1,31 @@
 from torch import nn, cat
 import torch.nn.functional as F
 
+# objects of class ConvBlock are initialized instead of normal conv2d layers, as all of them share the same batch normalization layer and activation function
+class ConvBlock():
 
-## conv_block significally cleans up code repetition, but at the cost of calling nn.Conv2d each time instead of it being called only once in the class initializer.
+    # dict contains all the information usually provided to Conv2d layers
+    def __init__(self,dict):
 
-def conv_block(x, dict):
+        self.conv = nn.Conv2d(
 
-    x = nn.Conv2d(
-        in_channels=dict['in_channels'], 
-        out_channels=dict['out_channels'], 
-        kernel_size=dict['kernel_size'], 
-        stride=dict['stride']
-                  )(x)
-                  
-    x = nn.BatchNorm2d(num_features=dict['out_channels'], eps=0.001)
-    x = F.relu(x) 
+            in_channels=dict['in_channels'], 
+            out_channels=dict['out_channels'], 
+            kernel_size=dict['kernel_size'], 
+            stride=dict['stride']
 
-    return x
+                  )
+        
+        self.bnorm = nn.BatchNorm2d(num_features=dict['out_channels'], eps=0.001)
+    
+    # fwd function is called in InceptionNet.foward() to tidy up the look and combat code repetition
+    def fwd(self,x):
+        
+        x = self.conv(x)
+        x = self.bnorm(x)
+        x = F.relu(x)
+
+
 
 
 class InceptionNet(nn.Module):
@@ -76,20 +85,20 @@ class InceptionNet(nn.Module):
     class Stem():
         def __init__(self):
 
-            self.conv_a1 = {'in_channels':3, 'out_channels':32, 'kernel_size':(3,3), 'stride':2}
-            self.conv_a2 = {'in_channels':32, 'out_channels':32, 'kernel_size':(3,3), 'stride':1}
-            self.conv_a3 = {'in_channels':32, 'out_channels':64, 'kernel_size':(3,3), 'stride':1}
-            self.conv_a4 = {'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':2}
+            self.conv_a1 = ConvBlock({'in_channels':3, 'out_channels':32, 'kernel_size':(3,3), 'stride':2})
+            self.conv_a2 = ConvBlock({'in_channels':32, 'out_channels':32, 'kernel_size':(3,3), 'stride':1})
+            self.conv_a3 = ConvBlock({'in_channels':32, 'out_channels':64, 'kernel_size':(3,3), 'stride':1})
+            self.conv_a4 = ConvBlock({'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':2})
 
-            self.conv_b1 = {'in_channels':160, 'out_channels':64, 'kernel_size':(1,1), 'stride':1}
-            self.conv_b2 = {'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':1}
+            self.conv_b1 = ConvBlock({'in_channels':160, 'out_channels':64, 'kernel_size':(1,1), 'stride':1})
+            self.conv_b2 = ConvBlock({'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':1})
 
-            self.conv_c1 = {'in_channels':160, 'out_channels':64, 'kernel_size':(1,1), 'stride':1}
-            self.conv_c2 = {'in_channels':64, 'out_channels':64, 'kernel_size':(7,1), 'stride':1}
-            self.conv_c3 = {'in_channels':64, 'out_channels':64, 'kernel_size':(1,7), 'stride':1}
-            self.conv_c4 = {'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':1}
+            self.conv_c1 = ConvBlock({'in_channels':160, 'out_channels':64, 'kernel_size':(1,1), 'stride':1})
+            self.conv_c2 = ConvBlock({'in_channels':64, 'out_channels':64, 'kernel_size':(7,1), 'stride':1})
+            self.conv_c3 = ConvBlock({'in_channels':64, 'out_channels':64, 'kernel_size':(1,7), 'stride':1})
+            self.conv_c4 = ConvBlock({'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':1})
 
-            self.conv_d1 = {'in_channels':192, 'out_channels':192, 'kernel_size':(3,3), 'stride':1}
+            self.conv_d1 = ConvBlock({'in_channels':192, 'out_channels':192, 'kernel_size':(3,3), 'stride':1})
 
             self.max_pool1 = nn.MaxPool2d(kernel_size=(3,3), stride=2)
             self.max_pool2 = nn.MaxPool2d(kernel_size=(3,3), stride=2)
@@ -99,30 +108,30 @@ class InceptionNet(nn.Module):
         def fwd(self,x):
 
             # input dim     (299 x 299 x 3)
-            x = conv_block(x,self.conv_a1)
+            x = self.conv_a1.fwd(x)
             # after conv1   (149 x 149 x 32)
-            x =  conv_block(x,self.conv_a2)
+            x = self.conv_a2.fwd(x)
             # after conv2   (147 x 147 x 32)
-            x =  conv_block(x,self.conv_a3)
+            x = self.conv_a3.fwd(x)
             # after conv3   (147 x 147 x 64)
             # splits left and right (x1,x2)
 
             x1 = self.max_pool1(x)
-            x2 =  conv_block(x,self.conv_a4)
+            x2 = self.conv_a4.fwd(x)
             x = cat((x1,x2),3)
             # after concatenation (73 x 73 x 160)
             # splits left and right (x1,x2)
 
-            x1 =  conv_block(x,self.conv_b1)
-            x1 =  conv_block(x1,self.conv_b2)
-            x2 =  conv_block(x,self.conv_c1)
-            x2 =  conv_block(x2,self.conv_c2)
-            x2 =  conv_block(x2,self.conv_c3)
-            x2 = conv_block(x2,self.conv_c4)
+            x1 = self.conv_b1.fwd(x)
+            x1 = self.conv_b2.fwd(x1)
+            x2 = self.conv_c1.fwd(x)
+            x2 = self.conv_c2.fwd(x2)
+            x2 = self.conv_c3.fwd(x2)
+            x2 = self.conv_c4.fwd(x2)
             x = cat((x1,x2),3)
             # after concatenation (71 x 71 x 192)
 
-            x1 = conv_block(x,self.conv_d1)
+            x1 = self.conv_d1.fwd(x)
             x2 = self.max_pool2(x)
             x = cat((x1,x2),3)
             # after concatenation (35 x 35 x 384)
@@ -133,32 +142,32 @@ class InceptionNet(nn.Module):
 
         def __init__(self):
 
-            self.conv_a1 = {'in_channels':384, 'out_channels':96, 'kernel_size':(1,1), 'stride':1} 
+            self.conv_a1 = ConvBlock({'in_channels':384, 'out_channels':96, 'kernel_size':(1,1), 'stride':1}) 
 
-            self.conv_b1 = {'in_channels':384, 'out_channels':96, 'kernel_size':(1,1), 'stride':1} 
+            self.conv_b1 = ConvBlock({'in_channels':384, 'out_channels':96, 'kernel_size':(1,1), 'stride':1}) 
 
-            self.conv_c1 = {'in_channels':384, 'out_channels':96, 'kernel_size':(1,1), 'stride':1} 
-            self.conv_c2 = {'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':1} 
+            self.conv_c1 = ConvBlock({'in_channels':384, 'out_channels':96, 'kernel_size':(1,1), 'stride':1}) 
+            self.conv_c2 = ConvBlock({'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':1})
 
-            self.conv_d1 = {'in_channels':384, 'out_channels':96, 'kernel_size':(1,1), 'stride':1} 
-            self.conv_d2 = {'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':1} 
-            self.conv_d3 = {'in_channels':96, 'out_channels':96, 'kernel_size':(3,3), 'stride':1} 
+            self.conv_d1 = ConvBlock({'in_channels':384, 'out_channels':96, 'kernel_size':(1,1), 'stride':1}) 
+            self.conv_d2 = ConvBlock({'in_channels':64, 'out_channels':96, 'kernel_size':(3,3), 'stride':1})
+            self.conv_d3 = ConvBlock({'in_channels':96, 'out_channels':96, 'kernel_size':(3,3), 'stride':1})
 
             self.avg_pool = nn.AvgPool2d(kernel_size=(3,3), stride=1, padding=1)
         
         def fwd(self,x):
 
             x1 = self.avg_pool(x)
-            x1 = conv_block(x1,self.conv_a1)
+            x1 = self.conv_a1.fwd(x1)
 
-            x2 = conv_block(x,self.conv_b1)
+            x2 = self.conv_b1.fwd(x2)
 
-            x3 = conv_block(x,self.conv_c1)
-            x3 = conv_block(x3,self.conv_c2)
+            x3 = self.conv_c1.fwd(x)
+            x3 = self.conv_c2.fwd(x3)
 
-            x4 = conv_block(x,self.conv_d1)
-            x4 = conv_block(x4,self.conv_d2)
-            x4 = conv_block(x4,self.conv_d3)
+            x4 = self.conv_d1.fwd(x)
+            x4 = self.conv_d2.fwd(x4)
+            x4 = self.conv_d3.fwd(x4)
 
             x = cat((x1,x2,x3,x4),3)
 
@@ -167,38 +176,38 @@ class InceptionNet(nn.Module):
     class InceptionB():
 
         def __init__(self):
-            self.conv_a1 = {'in_channels':1024, 'out_channels':128, 'kernel_size':(1,1), 'stride':1}            
+            self.conv_a1 = ConvBlock({'in_channels':1024, 'out_channels':128, 'kernel_size':(1,1), 'stride':1})            
 
-            self.conv_b1 = {'in_channels':1024, 'out_channels':384, 'kernel_size':(1,1), 'stride':1} 
+            self.conv_b1 = ConvBlock({'in_channels':1024, 'out_channels':384, 'kernel_size':(1,1), 'stride':1})
 
-            self.conv_c1 = {'in_channels':1024, 'out_channels':192, 'kernel_size':(1,1), 'stride':1} 
-            self.conv_c2 = {'in_channels':192, 'out_channels':224, 'kernel_size':(7,1), 'stride':1} 
-            self.conv_c3 = {'in_channels':224, 'out_channels':256, 'kernel_size':(1,7), 'stride':1} 
+            self.conv_c1 = ConvBlock({'in_channels':1024, 'out_channels':192, 'kernel_size':(1,1), 'stride':1})
+            self.conv_c2 = ConvBlock({'in_channels':192, 'out_channels':224, 'kernel_size':(7,1), 'stride':1})
+            self.conv_c3 = ConvBlock({'in_channels':224, 'out_channels':256, 'kernel_size':(1,7), 'stride':1})
 
-            self.conv_d1 = {'in_channels':1024, 'out_channels':192, 'kernel_size':(1,1), 'stride':1} 
-            self.conv_d2 = {'in_channels':192, 'out_channels':192, 'kernel_size':(1,7), 'stride':1} 
-            self.conv_d3 = {'in_channels':192, 'out_channels':224, 'kernel_size':(7,1), 'stride':1} 
-            self.conv_d4 = {'in_channels':224, 'out_channels':224, 'kernel_size':(1,7), 'stride':1} 
-            self.conv_d5 = {'in_channels':224, 'out_channels':256, 'kernel_size':(7,1), 'stride':1}
+            self.conv_d1 = ConvBlock({'in_channels':1024, 'out_channels':192, 'kernel_size':(1,1), 'stride':1}) 
+            self.conv_d2 = ConvBlock({'in_channels':192, 'out_channels':192, 'kernel_size':(1,7), 'stride':1})
+            self.conv_d3 = ConvBlock({'in_channels':192, 'out_channels':224, 'kernel_size':(7,1), 'stride':1})
+            self.conv_d4 = ConvBlock({'in_channels':224, 'out_channels':224, 'kernel_size':(1,7), 'stride':1})
+            self.conv_d5 = ConvBlock({'in_channels':224, 'out_channels':256, 'kernel_size':(7,1), 'stride':1})
 
             self.avg_pool = nn.AvgPool2d(kernel_size=(3,3), stride=1, padding=1)
         
         def fwd(self,x):
 
             x1 = self.avg_pool(x)
-            x1 = conv_block(x1,self.conv_a1)
+            x1 = self.conv_a1.fwd(x1)
 
-            x2 = conv_block(x,self.conv_b1)
+            x2 = self.conv_b1.fwd(x)
 
-            x3 = conv_block(x,self.conv_c1)
-            x3 = conv_block(x3,self.conv_c2)
-            x3 = conv_block(x3,self.conv_c3)
+            x3 = self.conv_c1.fwd(x)
+            x3 = self.conv_c2.fwd(x3)
+            x3 = self.conv_c3.fwd(x3)
 
-            x4 = conv_block(x,self.conv_d1)
-            x4 = conv_block(x4,self.conv_d2)
-            x4 = conv_block(x4,self.conv_d3)
-            x4 = conv_block(x4,self.conv_d4)
-            x4 = conv_block(x4,self.conv_d5)
+            x4 = self.conv_d1.fwd(x)
+            x4 = self.conv_d2.fwd(x4)
+            x4 = self.conv_d3.fwd(x4)
+            x4 = self.conv_d4.fwd(x4)
+            x4 = self.conv_d5.fwd(x4)
 
             x = cat((x1,x2,x3,x4),3) 
 
@@ -208,38 +217,38 @@ class InceptionNet(nn.Module):
          
         def __init__(self):
 
-            self.conv_a1 = {'in_channels':1536, 'out_channels':256, 'kernel_size':(1,1), 'stride':1}
+            self.conv_a1 = ConvBlock({'in_channels':1536, 'out_channels':256, 'kernel_size':(1,1), 'stride':1})
 
-            self.conv_b1 = {'in_channels':1536, 'out_channels':256, 'kernel_size':(1,1), 'stride':1}
+            self.conv_b1 = ConvBlock({'in_channels':1536, 'out_channels':256, 'kernel_size':(1,1), 'stride':1})
 
-            self.conv_c1 = {'in_channels':1536, 'out_channels':384, 'kernel_size':(1,1), 'stride':1}
-            self.conv_c_left = {'in_channels':384, 'out_channels':256, 'kernel_size':(1,3), 'stride':1}
-            self.conv_c_right = {'in_channels':384, 'out_channels':256, 'kernel_size':(3,1), 'stride':1}
+            self.conv_c1 = ConvBlock({'in_channels':1536, 'out_channels':384, 'kernel_size':(1,1), 'stride':1})
+            self.conv_c_left = ConvBlock({'in_channels':384, 'out_channels':256, 'kernel_size':(1,3), 'stride':1})
+            self.conv_c_right = ConvBlock({'in_channels':384, 'out_channels':256, 'kernel_size':(3,1), 'stride':1})
 
-            self.conv_d1 = {'in_channels':1536, 'out_channels':384, 'kernel_size':(1,1), 'stride':1}
-            self.conv_d2 = {'in_channels':384, 'out_channels':448, 'kernel_size':(1,3), 'stride':1}
-            self.conv_d3 = {'in_channels':448, 'out_channels':512, 'kernel_size':(3,1), 'stride':1}
-            self.conv_d_left = {'in_channels':512, 'out_channels':256, 'kernel_size':(3,1), 'stride':1}
-            self.conv_d_right = {'in_channels':512, 'out_channels':256, 'kernel_size':(1,3), 'stride':1}
+            self.conv_d1 = ConvBlock({'in_channels':1536, 'out_channels':384, 'kernel_size':(1,1), 'stride':1})
+            self.conv_d2 = ConvBlock({'in_channels':384, 'out_channels':448, 'kernel_size':(1,3), 'stride':1})
+            self.conv_d3 = ConvBlock({'in_channels':448, 'out_channels':512, 'kernel_size':(3,1), 'stride':1})
+            self.conv_d_left = ConvBlock({'in_channels':512, 'out_channels':256, 'kernel_size':(3,1), 'stride':1})
+            self.conv_d_right = ConvBlock({'in_channels':512, 'out_channels':256, 'kernel_size':(1,3), 'stride':1})
 
             self.avg_pool = nn.AvgPool2d(kernel_size=(3,3), stride=1, padding=1)
         
         def fwd(self,x):
 
             x1 = self.avg_pool(x)
-            x1 = conv_block(x1,self.conv_a1)
+            x1 = self.conv_a1.fwd(x1)
 
-            x2 = conv_block(x,self.conv_b1)
+            x2 = self.conv_b1.fwd(x)
 
-            x3 = conv_block(x,self.conv_c1)
-            x3_1 = conv_block(x3,self.conv_c_left)
-            x3_2 = conv_block(x3,self.conv_c_right)
+            x3 = self.conv_c1.fwd(x)
+            x3_1 = self.conv_c_left.fwd(x3)
+            x3_2 = self.conv_c_right.fwd(x3)
 
-            x4 = conv_block(x,self.conv_d1)
-            x4 = conv_block(x4,self.conv_d2)
-            x4 = conv_block(x4,self.conv_d3)
-            x4_1 = conv_block(x4,self.conv_d_left)
-            x4_2 = conv_block(x4,self.conv_d_right)
+            x4 = self.conv_d1.fwd(x)
+            x4 = self.conv_d2.fwd(x4)
+            x4 = self.conv_d3.fwd(x4)
+            x4_1 = self.conv_d_left.fwd(x4)
+            x4_2 = self.conv_d_right.fwd(x4)
 
             x = cat((x1,x2,x3_1,x3_2,x4_1,x4_2),3)
 
@@ -249,11 +258,11 @@ class InceptionNet(nn.Module):
         
         def __init__(self):
 
-            self.conv_a1 = {'in_channels':384, 'out_channels':384, 'kernel_size':(3,3), 'stride':2}
+            self.conv_a1 = ConvBlock({'in_channels':384, 'out_channels':384, 'kernel_size':(3,3), 'stride':2})
 
-            self.conv_b1 = {'in_channels':384, 'out_channels':192, 'kernel_size':(1,1), 'stride':1}
-            self.conv_b2 = {'in_channels':192, 'out_channels':224, 'kernel_size':(3,3), 'stride':1}
-            self.conv_b3 = {'in_channels':224, 'out_channels':256, 'kernel_size':(3,3), 'stride':2}
+            self.conv_b1 = ConvBlock({'in_channels':384, 'out_channels':192, 'kernel_size':(1,1), 'stride':1})
+            self.conv_b2 = ConvBlock({'in_channels':192, 'out_channels':224, 'kernel_size':(3,3), 'stride':1})
+            self.conv_b3 = ConvBlock({'in_channels':224, 'out_channels':256, 'kernel_size':(3,3), 'stride':2})
 
             self.max_pool = nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0)
 
@@ -261,11 +270,11 @@ class InceptionNet(nn.Module):
 
             x1 = self.max_pool(x)
 
-            x2 = conv_block(x,self.conv_a1)
+            x2 =self.conv_a1.fwd(x)
 
-            x3 = conv_block(x,self.conv_b1)
-            x3 = conv_block(x3,self.conv_b2)
-            x3 = conv_block(x3,self.conv_b3)
+            x3 = self.conv_b1.fwd(x)
+            x3 = self.conv_b2.fwd(x3)
+            x3 = self.conv_b3.fwd(x3)
             
             x = cat((x1,x2,x3),3)
 
@@ -275,13 +284,13 @@ class InceptionNet(nn.Module):
 
         def __init__(self):
 
-            self.conv_a1 = {'in_channels':1024, 'out_channels':192, 'kernel_size':(3,3), 'stride':1}
-            self.conv_a2 = {'in_channels':192, 'out_channels':192, 'kernel_size':(3,3), 'stride':2}
+            self.conv_a1 = ConvBlock({'in_channels':1024, 'out_channels':192, 'kernel_size':(3,3), 'stride':1})
+            self.conv_a2 = ConvBlock({'in_channels':192, 'out_channels':192, 'kernel_size':(3,3), 'stride':2})
 
-            self.conv_b1 = {'in_channels':1024, 'out_channels':256, 'kernel_size':(1,1), 'stride':1}
-            self.conv_b2 = {'in_channels':256, 'out_channels':256, 'kernel_size':(1,7), 'stride':1}
-            self.conv_b3 = {'in_channels':256, 'out_channels':320, 'kernel_size':(7,1), 'stride':1}
-            self.conv_b4 = {'in_channels':320, 'out_channels':320, 'kernel_size':(3,3), 'stride':2}
+            self.conv_b1 = ConvBlock({'in_channels':1024, 'out_channels':256, 'kernel_size':(1,1), 'stride':1})
+            self.conv_b2 = ConvBlock({'in_channels':256, 'out_channels':256, 'kernel_size':(1,7), 'stride':1})
+            self.conv_b3 = ConvBlock({'in_channels':256, 'out_channels':320, 'kernel_size':(7,1), 'stride':1})
+            self.conv_b4 = ConvBlock({'in_channels':320, 'out_channels':320, 'kernel_size':(3,3), 'stride':2})
 
             self.max_pool = nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0)
 
@@ -289,13 +298,13 @@ class InceptionNet(nn.Module):
 
             x1 = self.max_pool(x)
 
-            x2 = conv_block(x,self.conv_a1)
-            x2 = conv_block(x2,self.conv_a2)
+            x2 = self.conv_a1.fwd(x)
+            x2 = self.conv_a2.fwd(x2)
 
-            x3 = conv_block(x,self.conv_b1)
-            x3 = conv_block(x3,self.conv_b2)
-            x3 = conv_block(x3,self.conv_b3)
-            x3 = conv_block(x3,self.conv_b4)
+            x3 = self.conv_b1.fwd(x)
+            x3 = self.conv_b2.fwd(x3)
+            x3 = self.conv_b3.fwd(x3)
+            x3 = self.conv_b4.fwd(x3)
             
             x = cat((x1,x2,x3),3)
 
